df4f4768abafac181986323caa175aa42b0d8b3d
20100850-ganesh <113907987+20100850-ganesh@users.noreply.github.com>
2023-05-19 22:40:48 +0000

    Implemented genres api

---
+++ src/genres/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import express from 'express';
+import uniqid from 'uniqid';
+import { genres } from './genresData';
+
+const router = express.Router();
+router.get('/', (req, res) => {
+    res.json(genres);
+});
+
+// Get genre details
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (genres.id == id) {
+        res.status(200).json(genres);
+    } else {
+        res.status(404).json({
+            message: 'The resource you requested could not be found.',
+            status_code: 404
+        });
+    }
+});
+
+
+export default router;
