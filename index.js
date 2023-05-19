0152ace401502ab94521b3eba290f8cb46902353
20100850-ganesh <113907987+20100850-ganesh@users.noreply.github.com>
2023-05-19 22:09:23 +0000

    "Initial Express App"

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 /* eslint-disable no-undef */
-// Load the http module to create an http server.
-import http from 'http';
 import dotenv from 'dotenv';
-require('dotenv').config();
+import express from 'express';
+import moviesRouter from './src/movies';
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
-// Configure our HTTP server to respond with Hello World to all requests.
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("Hello EWD!");
+const app = express();
+
+const port = 8080;
+
+app.use('/api/movies', moviesRouter);
+
+app.listen(port, () => {
+    console.info(`Server running at ${port}`);
 });
-
-server.listen(port);
-
-// Put a friendly message on the terminal
-console.log(`Server running at ${port}`);
